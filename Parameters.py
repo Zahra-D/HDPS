fa05a4609342decf1b3419616b3193f5269cd7c3
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2024-09-29 21:43:40 +0330

    Update Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -1,22 +1,28 @@
-#Parameters initialization
+# Parameters initialization
+
 import torch
 import numpy as np
 
-T_FR = 67
-T_ER = 62
-T_LR = 70 
-T_D = 82
-
-T = 40
-J = 1000000
+## Life Cycle
 
 AGE_0 = 22
 
+T_ER = 62
+T_FR = 67
+T_LR = 70 
+T_D = 82
+
+T = T_ER - AGE_0
+
+## Initial Distribution
+
 P_EDU = 0.190156
+A_1 = 0 # asset for the first year
+THETA_0 = 0
+
+## Wage Function
 
 w_min = 5
-consumption_min = 2000
-
 
 BETA_w_0 = 1.6698
 BETA_w_1 = 0.0605
@@ -27,48 +33,44 @@ BETA_w_5 = -0.0002
 
 SIGMA_e = np.sqrt(.02601)
 
-THETA_0 = 0
-
-# PSI = 0.04
-
-PSI_R = 0.01
-T_R = 10
+## Preference
 
 BETA = 0.98803
 #generating all the beta_t, BETA_t: [41]
 
-
-
 ETA = 0.5
 GAMMA = 1.66
 
+PSI_R = 0.01
+PHI = 0.0006
+
+## Budget Constaint 
+
+consumption_min = 2000
 R = 0.042
 
+## Grids
 
-# asset for the first year
-A_1 = 0
+H = torch.tensor([0.0,1300.0,2080.0,2860.0]) #the defined work hours
 
-
-
-#the defined work hours
-H = torch.tensor([0.0,1300.0,2080.0,2860.0])
-
+## Tax Function
 
 KAPPA = 2.716084
 TAU = 0.1029
 
+## Social Security Function
+
 TAU_SS = 0.106
 W_b = torch.tensor(76200) 
-
-
-PHI = 0.0006
-
-T_S = 35
-
 BEND_POINTS_b = torch.tensor([0.9,0.32,0.15])
 BEND_POINTS_a = torch.tensor([6372,38422,76200])
-
-
-
-
 DELTA_t = torch.tensor([0.7, 0.75, 0.8, 0.866, 0.933, 1.00, 1.08, 1.16, 1.24])
+
+## Simulation
+
+J = 1000000
+
+## ?
+
+T_S = 35
+T_R = 10
