935756d50ba016c382a28c9e62810614c5a3f55a
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-01-11 21:49:16 +0330

    Update Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -3,7 +3,7 @@
 import torch
 import numpy as np
 
-## Life Cycle
+## Life Cycle --------------------
 
 AGE_0 = 22
 
@@ -14,13 +14,13 @@ T_D = 82
 
 T = T_ER - AGE_0
 
-## Initial Distribution
+## Initial Distribution --------------------
 
 P_EDU = 0.190156
 A_1 = 0 # asset for the first year
 THETA_0 = 0
 
-## Wage Function
+## Wage Function --------------------
 
 w_min = 5
 
@@ -33,7 +33,7 @@ BETA_w_5 = -0.0002
 
 SIGMA_e = np.sqrt(.02601)
 
-## Preference
+## Preference --------------------
 
 BETA = 0.98803
 #generating all the beta_t, BETA_t: [41]
@@ -44,21 +44,21 @@ GAMMA = 1.66
 PSI_R = 0.01
 PHI = 0.0006
 
-## Budget Constaint 
+## Budget Constaint -------------------- 
 
 consumption_min = 2000
 R = 0.042
 
-## Grids
+## Grids --------------------
 
 H = torch.tensor([0.0,1300.0,2080.0,2860.0]) #the defined work hours
 
-## Tax Function
+## Tax Function --------------------
 
 KAPPA = 2.716084
 TAU = 0.1029
 
-## Social Security Function
+## Social Security Function --------------------
 
 TAU_SS = 0.106
 W_b = torch.tensor(76200) 
@@ -66,7 +66,7 @@ BEND_POINTS_b = torch.tensor([0.9,0.32,0.15])
 BEND_POINTS_a = torch.tensor([6372,38422,76200])
 DELTA_t = torch.tensor([0.7, 0.75, 0.8, 0.866, 0.933, 1.00, 1.08, 1.16, 1.24])
 
-## Simulation
+## Simulation --------------------
 
 J = 1000000
 
