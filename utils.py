138dc90eaacde41c9d3cb569e4b9999873b5c030
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-01-25 09:39:07 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,39 @@
+## Defining the Objective Function --------------------
 
 from imports import *
-
 from functions import *
 from Parameters import *
 
-
 sns.set(color_codes=True)
 
+## Regularization Terms--------------------
+
+# Based only on last working year 
+
+# Workig year stage 
+
 def cal_regu_term_lastyear(model, T=40):
+
+  # Last year weigths
+  
   w_g_1_T = model.blocks[f'year_{T}'].general_layer_1.weight
   w_g_2_T = model.blocks[f'year_{T}'].general_layer_2.weight
-  
   b_g_1_T = model.blocks[f'year_{T}'].general_layer_1.bias
   b_g_2_T = model.blocks[f'year_{T}'].general_layer_2.bias
-
-
   w_h_T = model.blocks[f'year_{T}'].task_h.task_layer.weight
   w_a_T = model.blocks[f'year_{T}'].task_a_w.task_layer.weight
-  
   b_h_T = model.blocks[f'year_{T}'].task_h.task_layer.bias
   b_a_T = model.blocks[f'year_{T}'].task_a_w.task_layer.bias
 
+  # All the other years weigths
 
   general_regu = 0
   task_regu_a = 0
   task_regu_h = 0
-
-
+  
   for year in range(1,T):
-
-
-    num_input = year+1
+    
+    num_input = year + 1
 
     w_g_1_t = model.blocks[f'year_{year}'].general_layer_1.weight
     w_g_2_t = model.blocks[f'year_{year}'].general_layer_2.weight
@@ -39,14 +42,10 @@ def cal_regu_term_lastyear(model, T=40):
     general_regu += (torch.norm(w_g_1_T[:,:year+1] - w_g_1_t , 2) + torch.norm(w_g_2_T - w_g_2_t , 2)) / (T - year)
     general_regu += (torch.norm(b_g_1_T - b_g_1_t , 2) + torch.norm(b_g_2_T - b_g_2_t , 2)) / (T - year)
 
-
-
     w_h_t = model.blocks[f'year_{year}'].task_h.task_layer.weight
     b_h_t = model.blocks[f'year_{year}'].task_h.task_layer.bias
-    
     task_regu_h += torch.norm(w_h_T - w_h_t , 2) / (T - year)
     task_regu_h += torch.norm(b_h_T - b_h_t , 2) / (T - year)
-
 
     w_a_t = model.blocks[f'year_{year}'].task_layer_a_w.weight
     b_a_t = model.blocks[f'year_{year}'].task_layer_a_w.bias
@@ -55,68 +54,49 @@ def cal_regu_term_lastyear(model, T=40):
 
   return general_regu, task_regu_a, task_regu_h
 
+# Based on 10 year gaps before T_ER
 
-
-
-
-
+# Return work_retirement instead of work if t > T_ER
 def get_working_block(model, year):
   if year >= 62:
     return model.work_retirement_blocks[f'year_{year}'].working_block
   return model.work_blocks[f'year_{year}']
     
-
-def cal_regu_term_each10(model):
+def cal_regu_term_each10(model): # Only regulare general layer 1
 
   general_regu = 0
   retier_year = 0
 
-
   for year in range(AGE_0,T_LR):
     
-    if (((year-1)%10) == 0) or (year == 69):
+    if (((year-1)%10) == 0) or (year == 69): # Skips every 10th year or the last year
       continue
-    pin_year = (((year-1)//10)+1)*10 +1
+    pin_year = (((year-1)//10)+1)*10 +1 # Finds the closest previous 10-year anchor
     if year >= 62:
       # retier_year = 1
       pin_year = 69
     
-
-
-    
-    
-    
-    
     w_g_1_pin_year = get_working_block(model,pin_year).general_layer_1.weight
     # w_g_2_pin_year = get_working_block(model,pin_year).general_layer_2.weight
-    
     b_g_1_pin_year = get_working_block(model,pin_year).general_layer_1.bias
 
-    
-
     w_g_1_t = get_working_block(model,year).general_layer_1.weight
-
     b_g_1_t =  get_working_block(model,year).general_layer_1.bias
 
     general_regu += (torch.norm(w_g_1_pin_year[:,:year-AGE_0+3+retier_year] - w_g_1_t , 2)) / (pin_year - year)
     general_regu += (torch.norm(b_g_1_pin_year - b_g_1_t , 2)) / (pin_year - year)
 
-
-
-
   return general_regu
 
 
 
-
-def cal_regu_term_each10_old(model):
+def cal_regu_term_each10_old(model): # Regularize all layers
 
   general_regu = 0
   task_regu_a = 0
   task_regu_h = 0
   retier_year = 0
 
-
   for year in range(AGE_0,T_LR):
     
     if (((year-1)%10) == 0) or (year == 69):
@@ -126,44 +106,30 @@ def cal_regu_term_each10_old(model):
       # retier_year = 1
       pin_year = 69
     
-
-
-    
-    
-    
-    
     w_g_1_pin_year = get_working_block(model,pin_year).general_layer_1.weight
     w_g_2_pin_year = get_working_block(model,pin_year).general_layer_2.weight
-    
     b_g_1_pin_year = get_working_block(model,pin_year).general_layer_1.bias
     b_g_2_pin_year = get_working_block(model,pin_year).general_layer_2.bias
 
-
     w_h_pin_year = get_working_block(model,pin_year).task_h.task_layer.weight
     w_a_pin_year = get_working_block(model,pin_year).task_a_w.task_layer.weight
-    
     b_h_pin_year = get_working_block(model,pin_year).task_h.task_layer.bias
     b_a_pin_year = get_working_block(model,pin_year).task_a_w.task_layer.bias
     
-    
-    
-
     w_g_1_t = get_working_block(model,year).general_layer_1.weight
     w_g_2_t =  get_working_block(model,year).general_layer_2.weight
     b_g_1_t =  get_working_block(model,year).general_layer_1.bias
     b_g_2_t =  get_working_block(model,year).general_layer_2.bias
+    
     general_regu += (torch.norm(w_g_1_pin_year[:,:year-AGE_0+3+retier_year] - w_g_1_t , 2) + torch.norm(w_g_2_pin_year - w_g_2_t , 2)) / (pin_year - year)
     general_regu += (torch.norm(b_g_1_pin_year - b_g_1_t , 2) + torch.norm(b_g_2_pin_year - b_g_2_t , 2)) / (pin_year - year)
-
-
 
     w_h_t =  get_working_block(model,year).task_h.task_layer.weight
     b_h_t =  get_working_block(model,year).task_a_w.task_layer.bias
     
     task_regu_h += torch.norm(w_h_pin_year - w_h_t , 2) / (pin_year - year)
     task_regu_h += torch.norm(b_h_pin_year - b_h_t , 2) / (pin_year - year)
-
-
+    
     w_a_t =  get_working_block(model,year).task_h.task_layer.weight
     b_a_t =  get_working_block(model,year).task_a_w.task_layer.bias
     task_regu_a += torch.norm(w_a_pin_year - w_a_t , 2) / (pin_year - year)
@@ -172,14 +138,11 @@ def cal_regu_term_each10_old(model):
   return general_regu, task_regu_a, task_regu_h
 
 
+## Utility Computation --------------------
 
 
+def utility_retirement_pr(c_t, c_t_ER, pr_bar, pr_t, h_t, epoch, s_writer, args, mode = 'train'):
 
-
-  
-def utility_retirement_pr(c_t, c_t_ER, pr_bar, pr_t, h_t, epoch, s_writer, args, mode = 'train' ):
-
-    
   device = c_t.device
   BETA_t = torch.pow(BETA, torch.arange(T_D - AGE_0 + 1)).to(device)
   dummy_h = torch.zeros_like(c_t_ER[:, :,0]).to(device)
@@ -188,19 +151,15 @@ def utility_retirement_pr(c_t, c_t_ER, pr_bar, pr_t, h_t, epoch, s_writer, args,
   
   utility_ER = utility(c_t[:,:i_ER], h_t[:,:i_ER], BETA_t[:i_ER], args)
   
-
-
   utility_ww = utility(c_t_ER[:,:, 0], h_t[:,i_ER: i_LR +1], BETA_t[i_ER: i_LR+1], args)
   utility_rw = utility(c_t_ER[:,:, 1], dummy_h, BETA_t[i_ER: i_LR+1], args)
   utility_rr = utility(c_t_ER[:,:, 2], dummy_h, BETA_t[i_ER: i_LR+1], args)
   utility_LR = (1-pr_bar) * ( (1-pr_t) * utility_ww + pr_t * utility_rw )  +  pr_bar * (utility_rr)
   
-
   utility_D = utility(c_t[:,i_LR+1:], h_t[:,i_LR+1:], BETA_t[i_LR+1:],  args)
   total_utility =  utility_ER.sum(dim=-1) + utility_LR.sum(dim=-1) + utility_D.sum(dim=-1)
 
   return  total_utility
-
 
 def utility(c_t, h_t,BETA_t, args):
   consumption_utility =  (c_t**(1-GAMMA))/(1-GAMMA)
@@ -209,50 +168,47 @@ def utility(c_t, h_t,BETA_t, args):
   utility = (BETA_t * (consumption_utility - args.phi * working_disutility - args.psi * work_hour_disutility))
   return utility
 
-  
-  
 
+ ## Loss Function --------------------
   
 def loss_function_retirement_pr_cross(model, c_t, c_t_ER, pr_bar, pr_t ,h_t, epoch, s_writer, args):
 
+  # Life Time Utility 
   util = utility_retirement_pr( c_t, c_t_ER, pr_bar, pr_t, h_t, epoch, s_writer, args) 
+  
+  # Regurization terms
   if args.reg_mode == 'each10':
       cal_regu_term = cal_regu_term_each10
   elif args.reg_mode == 'last_year':
       cal_regu_term = cal_regu_term_lastyear
   general_regu =  cal_regu_term(model)
   # general_regu, task_regu_a, task_regu_h =  cal_regu_term(model)
-
   
+  # Weights
   
-  
-  
-  # l_G= l_h = l_a  = args.lmbd
+  # l_G = l_h = l_a  = args.lmbd
   # l_U = 1 - (l_G + l_a+ l_h + l_a)
-  # reg_term = l_G * general_regu + l_h * task_regu_h + l_a * task_regu_a 
-  # loss = -1 * l_U * util.mean() + reg_term 
     
-  l_G=  args.lmbd
+  l_G =  args.lmbd
   l_U = 1 - (l_G)
+
+  # Objective FUnction
+
   reg_term = l_G * general_regu 
+  # reg_term = l_G * general_regu + l_h * task_regu_h + l_a * task_regu_a 
   loss = -1 * l_U * util.mean() + reg_term 
 
+  # Tracking Training Progress
   
-  
-
   s_writer.add_scalar('Loss/reg_term',reg_term.detach().cpu(), epoch)
   s_writer.add_scalar('Loss/util_term',util.mean().detach().cpu(), epoch)
   # s_writer.add_scalar('Loss/r_term',loss_R.detach().cpu(), epoch)
   
-  
   return loss  
   
   
-  
-  
+## Generating the simulated exogenous state vars dataset  --------------------
 
-
-  
   
 def generating_dataset(number_samples, duration, theta_0, p_edu):
   
@@ -263,34 +219,30 @@ def generating_dataset(number_samples, duration, theta_0, p_edu):
     u_t = mu(edu.unsqueeze(1), torch.arange(1,duration+1))
     w_t = wage(u_t, theta_t)
 
-    
     return TensorDataset(theta_t, w_t, edu)
    
   
-  
-
+# Run Analyis --------------------
 
 
 def save_checkpoint(model, optimizer, base_dir, epoch):
     
-    pathlib.Path(f'{base_dir}/epoch{epoch}').mkdir(parents=True)
-
-
-                
-    torch.save(optimizer.state_dict(), f'{base_dir}/epoch{epoch}/optimizer_state.pth')
-    rng_checkpoint = {
+    pathlib.Path(f'{base_dir}/epoch{epoch}').mkdir(parents=True) # Folder of a run and epoch
+           
+    torch.save(optimizer.state_dict(), f'{base_dir}/epoch{epoch}/optimizer_state.pth') # Save optimzer's state
+    
+    rng_checkpoint = { # Random seed
         'torch_rng_state':
-            torch.get_rng_state(),
+            torch.get_rng_state(), # PyTorch’s RNG (CPU).
         'cuda_rng_state':
-            torch.cuda.get_rng_state_all(),
+            torch.cuda.get_rng_state_all(), # PyTorch’s CUDA RNG (for GPU).
         'numpy_rng_state':
-            np.random.get_state(),
+            np.random.get_state(), # NumPy’s random state.
         'python_rng_state':
-            random.getstate()
+            random.getstate() # Python’s built-in random module state.
     }
-    
     with open(f'{base_dir}/epoch{epoch}/rng_checkpoint.pkl', 'wb') as f:
-        pickle.dump(rng_checkpoint, f)
+        pickle.dump(rng_checkpoint, f) # Save Random Seeds
     
-    torch.save(model, f"{base_dir}/epoch{epoch}/model.pt")
+    torch.save(model, f"{base_dir}/epoch{epoch}/model.pt") # Save Model architercture and parameteres
 
