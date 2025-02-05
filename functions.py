a49d24bb473f8eee18a93680a9d989035cc5a015
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-02-05 20:02:31 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
 ### Defining functions used in the model
 
+
 from imports import *
 from Parameters import *
 
+
 ## Wage Function --------------------
+
 
 # Deterministic part
 mu = lambda edu, t: BETA_w_0 + BETA_w_1 * (t+AGE_0) +  BETA_w_2 * (t+AGE_0)**2 + BETA_w_3 * edu + BETA_w_4 * edu * (t+AGE_0) + BETA_w_5 * edu * (t+AGE_0)**2
@@ -13,11 +16,15 @@ theta = lambda theta_pre,e: theta_pre + e   # Update the persistant part (Markov
 # Wage (stochstic part+determisnitic part), accouting for min wage
 wage = lambda mu_t, theta_t : torch.maximum(torch.e**(mu_t + theta_t) , torch.tensor(w_min))
 
+
 ## Budget Constaint --------------------
+
 
 consumption = lambda a_t, y_t : a_t[:, :-1] + y_t -  a_t[:, 1:]/(1+R)
 
+
 ## Public Policies --------------------
+
 
 income_tax  = lambda y_t: y_t - KAPPA * (y_t+1e-8)**(1-TAU)
 
