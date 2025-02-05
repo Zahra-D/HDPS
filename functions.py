57781d6e2f98c59171a3236a71628cee020164d7
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-02-05 19:54:21 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,21 +1,23 @@
-## Defining functions used in the model
+### Defining functions used in the model
 
 from imports import *
 from Parameters import *
 
-## Wage Function
+## Wage Function --------------------
 
 # Deterministic part
 mu = lambda edu, t: BETA_w_0 + BETA_w_1 * (t+AGE_0) +  BETA_w_2 * (t+AGE_0)**2 + BETA_w_3 * edu + BETA_w_4 * edu * (t+AGE_0) + BETA_w_5 * edu * (t+AGE_0)**2
 # Stochastic part 
 e = lambda n: torch.normal(0, SIGMA_e, n) # Shcoks to persistant part
 theta = lambda theta_pre,e: theta_pre + e   # Update the persistant part (Markov d1)
-# Wage (stochstic part+determisnitic part),accouting for min wage
+# Wage (stochstic part+determisnitic part), accouting for min wage
 wage = lambda mu_t, theta_t : torch.maximum(torch.e**(mu_t + theta_t) , torch.tensor(w_min))
 
-## Budget Constaint
+## Budget Constaint --------------------
 
 consumption = lambda a_t, y_t : a_t[:, :-1] + y_t -  a_t[:, 1:]/(1+R)
+
+## Public Policies --------------------
 
 income_tax  = lambda y_t: y_t - KAPPA * (y_t+1e-8)**(1-TAU)
 
@@ -30,7 +32,6 @@ def retirement_benefit(all_y, t_R, TS=35):
     delta = delta_t[t_R]
     S = Score(all_y, TS)
     b = pension_benefit(S) * delta
-    
     return b
 
 # ?
