49a314a8f41ec30a81474cb3d6b0a56de254979f
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-02-06 16:36:35 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -30,10 +30,12 @@ income_tax  = lambda y_t: y_t - KAPPA * (y_t+1e-8)**(1-TAU)
 
 social_security_tax = lambda y_t: TAU_SS * torch.minimum(y_t, W_b)
 Score = lambda y_t, T_S: torch.topk(y_t, T_S).values.mean(dim=-1)
+
 pension_benefit = lambda S: ((BEND_POINTS_b[0] * S * (S <= BEND_POINTS_a[0])) +
                              (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (S - BEND_POINTS_a[0])) * ((BEND_POINTS_a[0] < S) & (S <= BEND_POINTS_a[1])) +
                              (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (BEND_POINTS_a[1] - BEND_POINTS_a[0]) + BEND_POINTS_b[2] * (S - BEND_POINTS_a[1])) * ((BEND_POINTS_a[1] < S) & (S <= BEND_POINTS_a[2])) +
                              (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (BEND_POINTS_a[1] - BEND_POINTS_a[0]) + BEND_POINTS_b[2] * (W_b - BEND_POINTS_a[1])) * (S > BEND_POINTS_a[2]))
+
 def retirement_benefit(all_y, t_R, TS=35):
     delta_t = DELTA_t.to(all_y.device)
     delta = delta_t[t_R]
