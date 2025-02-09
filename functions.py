455f6faa1325fb3aea98e5f46ed59228d3f2ef61
Sahber Ahmadi Renani <ahmadirenani@wisc.edu>
2025-02-10 02:20:17 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -26,19 +26,19 @@ consumption = lambda a_t, y_t : a_t[:, :-1] + y_t -  a_t[:, 1:]/(1+R)
 ## Public Policies --------------------
 
 
-income_tax  = lambda y_t: y_t - KAPPA * (y_t+1e-8)**(1-TAU)
+income_tax  = lambda y_t: y_t - KAPPA_Inc_Tax * (y_t+1e-8)**(1-TAU_Inc_Tax)
 
-social_security_tax = lambda y_t: TAU_SS * torch.minimum(y_t, W_b)
+social_security_tax = lambda y_t: TAU_SS_Tax * torch.minimum(y_t, Inc_base_SS)
 Score = lambda y_t, T_S: torch.topk(y_t, T_S).values.mean(dim=-1)
 
-pension_benefit = lambda S: ((BEND_POINTS_b[0] * S * (S <= BEND_POINTS_a[0])) +
-                             (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (S - BEND_POINTS_a[0])) * ((BEND_POINTS_a[0] < S) & (S <= BEND_POINTS_a[1])) +
-                             (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (BEND_POINTS_a[1] - BEND_POINTS_a[0]) + BEND_POINTS_b[2] * (S - BEND_POINTS_a[1])) * ((BEND_POINTS_a[1] < S) & (S <= BEND_POINTS_a[2])) +
-                             (BEND_POINTS_b[0] * BEND_POINTS_a[0] + BEND_POINTS_b[1] * (BEND_POINTS_a[1] - BEND_POINTS_a[0]) + BEND_POINTS_b[2] * (W_b - BEND_POINTS_a[1])) * (S > BEND_POINTS_a[2]))
+pension_benefit = lambda S: ((BEND_POINTS_b_SS[0] * S * (S <= BEND_POINTS_a_SS[0])) +
+                             (BEND_POINTS_b_SS[0] * BEND_POINTS_a_SS[0] + BEND_POINTS_b_SS[1] * (S - BEND_POINTS_a_SS[0])) * ((BEND_POINTS_a_SS[0] < S) & (S <= BEND_POINTS_a_SS[1])) +
+                             (BEND_POINTS_b_SS[0] * BEND_POINTS_a_SS[0] + BEND_POINTS_b_SS[1] * (BEND_POINTS_a_SS[1] - BEND_POINTS_a_SS[0]) + BEND_POINTS_b_SS[2] * (S - BEND_POINTS_a_SS[1])) * ((BEND_POINTS_a_SS[1] < S) & (S <= BEND_POINTS_a_SS[2])) +
+                             (BEND_POINTS_b_SS[0] * BEND_POINTS_a_SS[0] + BEND_POINTS_b_SS[1] * (BEND_POINTS_a_SS[1] - BEND_POINTS_a_SS[0]) + BEND_POINTS_b_SS[2] * (Inc_base_SS - BEND_POINTS_a_SS[1])) * (S > BEND_POINTS_a_SS[2]))
 
 def retirement_benefit(all_y, t_R, TS=35):
     delta_t = DELTA_t.to(all_y.device)
-    delta = delta_t[t_R]
+    delta = DELTA_t_SS[t_R]
     S = Score(all_y, TS)
     b = pension_benefit(S) * delta
     return b
